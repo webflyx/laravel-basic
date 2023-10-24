9120469c52aa80dd82ace4fbba66331792035954
webflyx <zavala180395@gmail.com>
2023-10-24 22:06:48 +0300

    Added migrations

--- database/migrations/2023_10_24_165948_create_users_table.php
+++ database/migrations/2023_10_24_165948_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->boolean('active')->default(true)->comment('Block or active user');
+
             $table->timestamps();
+            $table->string('password');
+            $table->rememberToken(); //For "remember me" by login
         });
     }
 
